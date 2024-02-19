82c2ef04285afcac8c024d0b9e50c5848b0a2fc1
Ggirassol <gi-12@hotmail.com>
2024-02-19 16:34:46 +0000

    CORE: GET /api/articles/:article_id completed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getTopics, getDescription } = require("./controllers");
+const { getTopics, getDescription, getArticleById } = require("./controllers");
 const app = express();
 
 
@@ -8,10 +8,25 @@ app.get("/api/topics", getTopics)
 
 app.get("/api", getDescription)
 
+app.get("/api/articles/:article_id", getArticleById)
+
 
 
 app.get("*", (req,res) => {
     res.status(404).send({ msg: 'Page not found' });
+})
+
+app.use((err, req, res, next) => {
+    if (err.code === '22P02') {
+      res.status(400).send({ msg: 'Bad request' })
+    }
+    next(err)
+  });
+
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send( {msg: err.msg} )
+    }
 })
 
 app.use((err, req, res, next) => {
