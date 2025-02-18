af1ec27aaa41ef211308b9574f5c76171141c184
Ggirassol <gi-12@hotmail.com>
2025-02-18 07:55:42 +0000

    db migration

--- db/connection.js
+++ db/connection.js
@@ -9,11 +9,37 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-const config = {};
+let config = {};
+
+const fs = require("fs");
+const pg = require("pg");
 
 if (ENV === 'production') {
-  config.connectionString = process.env.DATABASE_URL;
+  config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: 10026,
+    database: "defaultdb",
+    ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync("db/seeds/ca.pem").toString(),
+    },
+  };
   config.max = 2;
 }
+
+const client = new pg.Client(config);
+client.connect(function (err) {
+  if (err) throw err;
+  client.query("SELECT VERSION()", [], function (err, result) {
+    if (err) throw err;
+    console.log(result.rows[0]);
+    client.end(function (err) {
+      if (err) throw err;
+    });
+  });
+});
+
 
 module.exports = new Pool(config);
